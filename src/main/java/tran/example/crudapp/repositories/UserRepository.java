9857edf8a112c1471e5920b5337a59b53bdcb9a6
jstran9 <wptran58@gmail.com>
2019-05-09 19:01:39 -0700

    Repository used for CRUD functionality.

---
+++ src/main/java/tran/example/crudapp/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package tran.example.crudapp.repositories;
+
+import tran.example.crudapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByName(String name);
+}
